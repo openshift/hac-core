0e1ae8cef08ec4955a08afaa2b22bab1f297f919
Karel Hala <khala@redhat.com>
2021-11-16 10:18:52 +0100

    Allow sharing of react i18 next to support dynamic plugins (#19)

--- src/poc-code/console-dynamic-plugin-sdk/src/shared-modules.ts
+++ src/poc-code/console-dynamic-plugin-sdk/src/shared-modules.ts
@@ -6,10 +6,10 @@ export const sharedPluginModules = [
   // '@openshift-console/dynamic-plugin-sdk-internal',
   // '@openshift-console/dynamic-plugin-sdk-internal-kubevirt',
   '@patternfly/react-core',
-  // '@patternfly/react-table',
+  '@patternfly/react-table',
   'react',
-  // 'react-helmet',
-  // 'react-i18next',
+  'react-helmet',
+  'react-i18next',
   'react-router',
   'react-router-dom',
 ];
