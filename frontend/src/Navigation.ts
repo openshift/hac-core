fc6c195508438bba8fbcfc4b108b8430f3dfee01
Karel Hala <khala@redhat.com>
2021-11-24 16:53:00 +0100

    Add proper type guards and error statuses based on review

--- frontend/src/Navigation.ts
+++ frontend/src/Navigation.ts
@@ -1,6 +1,8 @@
 import { activePlugins } from './Utils/constants';
 import { HrefNavItem, NavSection } from '@console/dynamic-plugin-sdk/src';
 import { EnabledPlugin } from '@console/mount/src/components/plugins/IncludePlugins';
+import { Extension } from '@console/dynamic-plugin-sdk/src/types';
+import { ConsolePluginManifestJSON } from '@console/dynamic-plugin-sdk/src/schema/plugin-manifest';
 
 export interface RouteProps {
   isHidden?: boolean;
@@ -14,26 +16,38 @@ export interface DynamicNav {
   currentNamespace: string;
 }
 
-export type GetAllExtensions = () => Promise<(HrefNavItem | NavSection)[]>;
+const navExtensionTypes = ['console.navigation/href', 'console.navigation/section'];
+type NavExtension = HrefNavItem | NavSection;
+
+export type GetAllExtensions = () => Promise<NavExtension[]>;
 export type CalculateRoutes = (navIdentifier: [string, string], currentNamespace: string, extensions: (HrefNavItem | NavSection)[]) => RouteProps[];
 
-const getAllExtensions: GetAllExtensions = async () => {
-  const extensions = (await Promise.allSettled(
-    activePlugins.flatMap(async ({ name: pluginName, pathPrefix = '/api/plugins' }: EnabledPlugin) => {
-      try {
-        const manifest = await (await fetch(`${pathPrefix}/${pluginName}/plugin-manifest.json`))?.json();
-        return manifest?.extensions;
-      } catch (e) {
-        console.log(e);
-      }
-    }),
-  ));
+const isFulfilledPromise = (result: PromiseSettledResult<Extension[]>): result is PromiseFulfilledResult<Extension[]> => {
+    return result.status === 'fulfilled' && Boolean(result.value);
+};
 
-  return extensions
-  .filter((result): result is PromiseFulfilledResult<(HrefNavItem | NavSection)[]> => result.status === 'fulfilled' && result.value)
+const isNavItem = (extension: Extension): extension is NavExtension => {
+    return navExtensionTypes.includes(extension.type);
+};
+
+const getAllExtensions: GetAllExtensions = async () => {
+    const results: PromiseSettledResult<Extension[]>[] = await Promise.allSettled(
+        activePlugins.flatMap(async ({ name: pluginName, pathPrefix = '/api/plugins' }: EnabledPlugin) => {
+            const url = `${pathPrefix}/${pluginName}/plugin-manifest.json`;
+            const response: Response = await fetch(url);
+            if (response.status !== 200) {
+                throw new Error(`${url} - ${response.status} - ${response.statusText}`);
+            }
+            const manifest: ConsolePluginManifestJSON = await response.json();
+            return manifest.extensions;
+        }),
+    );
+
+  return results
+  .filter(isFulfilledPromise)
   .map(({ value }) => value)
   .flat()
-  .filter(({ type }) => ['console.navigation/href', 'console.navigation/section'].includes(type));
+  .filter(isNavItem);
 };
 
 const calculateRoutes: CalculateRoutes = ([appId, navSection], currentNamespace, extensions) => {
@@ -48,13 +62,15 @@ const calculateRoutes: CalculateRoutes = ([appId, navSection], currentNamespace,
 
 export default async ({ dynamicNav, currentNamespace }: DynamicNav) => {
   const [appId, navSection] = dynamicNav.split('/');
-  let allExtensions = [];
+  let allExtensions: NavExtension[] = [];
   let routes: RouteProps | RouteProps[];
   try {
     allExtensions = await getAllExtensions();
     routes = calculateRoutes([appId, navSection], currentNamespace, allExtensions);
-  } catch {
+  } catch (e) {
     routes = [{ isHidden: true }];
+    // eslint-disable-next-line no-console
+    console.error('Problem fetching extensions', e);
   }
   const { properties: currSection } =
     allExtensions.find(({ type, properties }: NavSection) => type === 'console.navigation/section' && properties.id === navSection) || {};
