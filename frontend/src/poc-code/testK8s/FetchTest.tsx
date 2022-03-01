d1bfd7026a2b72c4950a0069feac7b9fb7621136
Andrew Ballantyne <aballant@redhat.com>
2022-03-01 15:04:21 -0500

    Rework Test Pages

---
+++ frontend/src/poc-code/testK8s/FetchTest.tsx
@@ -0,0 +1,217 @@
+import * as React from 'react';
+import { Button, TextInput, Title } from '@patternfly/react-core';
+import PrintObject from './PrintObject';
+import {
+  k8sCreateResource,
+  k8sDeleteResource,
+  k8sGetResource,
+  k8sListResource,
+  K8sModel,
+  k8sPatchResource,
+  K8sResourceCommon,
+  k8sUpdateResource,
+} from './dynamic-plugin-sdk';
+
+const ProjectModel: K8sModel = {
+  apiVersion: 'v1',
+  apiGroup: 'project.openshift.io',
+  kind: 'Project',
+  abbr: 'PR',
+  label: 'Project',
+  labelPlural: 'Projects',
+  plural: 'projects',
+};
+// const ConfigMapModel: K8sModel = {
+//   apiVersion: 'v1',
+//   kind: 'ConfigMap',
+//   abbr: 'CM',
+//   plural: 'configmaps',
+//   labelPlural: 'ConfigMaps',
+//   label: 'ConfigMap',
+//   namespaced: true,
+// };
+
+const ApplicationModel: K8sModel = {
+  apiVersion: 'v1alpha1',
+  kind: 'Application',
+  apiGroup: 'appstudio.redhat.com',
+  abbr: 'A',
+  label: 'Application',
+  labelPlural: 'Applications',
+  plural: 'applications',
+};
+
+// eslint-disable-next-line no-shadow
+enum ActionType {
+  LIST = 'list projects',
+  CREATE = 'create',
+  GET = 'get',
+  PATCH = 'patch',
+  PUT = 'put',
+  DELETE = 'delete',
+}
+
+const FetchTest: React.FC = () => {
+  const [r, setR] = React.useState(null);
+  const [namespace, setNamespace] = React.useState<string>('default');
+  const [name, setName] = React.useState<string>('test');
+  const [status, setStatus] = React.useState<string>('');
+  const [action, setAction] = React.useState<ActionType | null>(null);
+
+  // const result = useK8sWatchResource({
+  //   groupVersionKind: { version: 'v1', kind: 'ConfigMap' },
+  //   name: 'test',
+  //   namespace,
+  // });
+  // const result = useK8sWatchResource(
+  //   namespace === 'default' // default namespace has 100s of configmaps, don't fetch it
+  //     ? {}
+  //     : {
+  //         // groupVersionKind: { group: 'project.openshift.io', version: 'v1', kind: 'Project' },
+  //         groupVersionKind: { version: 'v1', kind: 'ConfigMap' },
+  //         isList: true,
+  //         namespace,
+  //       },
+  // );
+  // const hookResource = result[0];
+  // console.debug('render result', result);
+
+  React.useEffect(() => {
+    const testApplicationMetadata = {
+      metadata: {
+        name,
+        namespace,
+      },
+    };
+    const testApplicationData: K8sResourceCommon & { [key: string]: any } = {
+      apiVersion: `${ApplicationModel.apiGroup}/${ApplicationModel.apiVersion}`,
+      kind: ApplicationModel.kind,
+      ...testApplicationMetadata,
+      spec: {
+        displayName: name,
+      },
+    };
+
+    let promise = null;
+    switch (action) {
+      case ActionType.LIST:
+        // TODO: this can work sorta for getting your namespace value
+        // response[0].metadata.name === your namespace
+        promise = k8sListResource({
+          model: ProjectModel,
+        }).then((data) => {
+          // Lock in the namespace
+          let ns = null;
+          if (Array.isArray(data)) {
+            const namespaces = data.map((dataResource) => dataResource.metadata.name);
+            console.debug('++++available namespaces:', namespaces);
+            ns = namespaces[0];
+          } else if (data?.metadata?.namespace) {
+            ns = data.metadata.namespace;
+          }
+
+          if (ns) {
+            setAction(null); // prevent re-invoking this effect/call
+            setNamespace(ns);
+          } else {
+            // eslint-disable-next-line no-alert
+            alert('Could not find namespace; you are likely not able to do much as we are targeting "default"');
+          }
+          return data;
+        });
+        break;
+      case ActionType.CREATE:
+        promise = k8sCreateResource({
+          model: ApplicationModel,
+          data: testApplicationData,
+        });
+        break;
+      case ActionType.GET:
+        promise = k8sGetResource({
+          model: ApplicationModel,
+          name: testApplicationMetadata.metadata.name,
+          ns: testApplicationMetadata.metadata.namespace,
+        });
+        break;
+      case ActionType.PATCH:
+        promise = k8sPatchResource({
+          model: ApplicationModel,
+          resource: testApplicationMetadata,
+          data: [
+            {
+              op: 'replace',
+              path: '/data/test',
+              value: 'false',
+            },
+          ],
+        });
+        break;
+      case ActionType.PUT:
+        promise = k8sUpdateResource({
+          model: ApplicationModel,
+          name: testApplicationMetadata.metadata.name,
+          ns: testApplicationMetadata.metadata.namespace,
+          data: { ...testApplicationData, data: { ...testApplicationData.data, new: 'prop' } },
+        });
+        break;
+      case ActionType.DELETE:
+        promise = k8sDeleteResource({
+          model: ApplicationModel,
+          resource: testApplicationMetadata,
+        });
+        break;
+      case null:
+        // ignore effect
+        break;
+      default:
+        // this shouldn't happen, catch state for missed cases
+        throw new Error('uh oh!');
+    }
+    promise
+      ?.then((data) => {
+        setStatus(`${action} response:`);
+        setR(data);
+        console.debug(`++++${action}!`, data);
+      })
+      .catch((err) => {
+        console.error(`++++failed ${action}`, err);
+        setStatus(`failed call: ${err.message}`);
+        setR(null);
+      })
+      .finally(() => {
+        setAction(null); // prevent the hook for re-firing on name change
+      });
+  }, [action, name, namespace]);
+
+  return (
+    <>
+      <Title headingLevel="h2" size="xl">
+        Fetch Calls
+      </Title>
+      <TextInput placeholder="Application name" onChange={(v) => setName(v)} value={name} />
+      <div>
+        <p>Test calls -- predefined data; use the above input to make/update/get multiple Applications</p>
+        {Object.values(ActionType).map((v) => (
+          <React.Fragment key={v}>
+            <Button isDisabled={v !== ActionType.LIST && namespace === 'default'} onClick={() => setAction(v)}>
+              {v}
+            </Button>{' '}
+          </React.Fragment>
+        ))}
+        In `{namespace}` namespace
+      </div>
+      {/*{hookResource ? (*/}
+      {/*  <>*/}
+      {/*    {Array.isArray(hookResource) && <p>{hookResource.length} item(s) in the array</p>}*/}
+      {/*    <pre>{JSON.stringify(hookResource, null, 2)}</pre>*/}
+      {/*  </>*/}
+      {/*) : (*/}
+      {/*  'No watched resource'*/}
+      {/*)}*/}
+      <div>{status}</div>
+      {r && <PrintObject object={r} />}
+    </>
+  );
+};
+
+export default FetchTest;
--- frontend/src/poc-code/testK8s/WSTest.tsx
+++ frontend/src/poc-code/testK8s/WSTest.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import * as React from 'react';
 import { WSFactory } from './ws-factory';
+import PrintObject from './PrintObject';
+import { Alert, Button, Spinner, Title } from '@patternfly/react-core';
 
 const getToken = (): string | null => {
   const cookieToken = document.cookie.split('; ').find((val) => val.startsWith('cs_jwt='));
@@ -19,56 +21,78 @@ const base64 = (data: string): string => {
 };
 
 const WSTest = () => {
-  const ref = React.useRef<WSFactory>(null);
+  const [r, setR] = React.useState(null);
+  const [createSocket, setCreateSocket] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
   // To get WS to work even a bit, we need to modify @redhat-cloud-services/frontend-components-config-utilities
   // in 'frontend/node_modules/@redhat-cloud-services/frontend-components-config-utilities/repos/insights-chrome-build/snippets/head.html'
   // 'wss:' needs to exist next to 'https:'
   React.useEffect(() => {
-    console.debug('+++++++hook start+++++++');
     const token = getToken();
 
+    let ws: WSFactory;
     if (token) {
-      const base64Token = base64(token);
+      if (createSocket) {
+        const base64Token = base64(token);
 
-      ref.current = new WSFactory('sample websocket', {
-        host: 'wss://api-toolchain-host-operator.apps.appstudio-stage.x99m.p1.openshiftapps.com:443',
-        path: `/apis/appstudio.redhat.com/v1alpha1/namespaces/aballantyne/applications?watch=true`,
-        subprotocols: [`base64url.bearer.authorization.k8s.io.${base64Token}`, 'base64.binary.k8s.io'],
-      });
-      ref.current.onerror((data) => {
-        console.debug('error', data);
-      });
-      ref.current.onmessage((data) => {
-        console.debug('message', data);
-      });
-      ref.current.onclose((data) => {
-        // https://www.rfc-editor.org/rfc/rfc6455#section-11.7
-        // 1006: https://stackoverflow.com/a/19305172
-        console.debug('close', data, data.code);
-      });
-      ref.current.onbulkmessage((data) => {
-        console.debug('bulkmessage', data);
-      });
-      ref.current.onopen((data) => {
-        console.debug('open', data);
-      });
-      ref.current.ondestroy((data) => {
-        console.debug('destroy', data);
-      });
+        ws = new WSFactory('sample websocket', {
+          host: 'wss://api-toolchain-host-operator.apps.appstudio-stage.x99m.p1.openshiftapps.com:443',
+          path: `/apis/appstudio.redhat.com/v1alpha1/namespaces/aballantyne/applications?watch=true`,
+          subprotocols: [`base64url.bearer.authorization.k8s.io.${base64Token}`, 'base64.binary.k8s.io'],
+        });
+        ws.onerror((data) => {
+          console.debug('error', data);
+        });
+        ws.onmessage((dataString) => {
+          try {
+            const data = JSON.parse(dataString);
+            const { type, object } = data || {}; // current structure
+            setR(object);
+            console.debug('message', type, object);
+          } catch (e) {
+            console.error('>>> Web Socket Data Bad', e);
+            setError('Web Socket data unknown structure');
+            return;
+          }
+        });
+        ws.onclose((data) => {
+          setR(null);
+          setCreateSocket(null);
+          // https://www.rfc-editor.org/rfc/rfc6455#section-11.7
+          // 1006: https://stackoverflow.com/a/19305172
+          console.debug('close', data, 'code:', data.code);
+        });
+      }
     } else {
-      console.debug('>>>>>>>>failed to get token');
+      setError('Unknown token');
     }
 
     return () => {
-      console.debug('------hook unload------');
-      ref.current.destroy();
-      ref.current = null;
+      ws?.destroy();
+      ws = null;
     };
-  }, []);
+  }, [createSocket]);
 
-  // TODO: Print watched resource
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Title headingLevel="h2" size="xl">
+        Websockets
+      </Title>
+      {!createSocket && (
+        <Button onClick={() => setCreateSocket(true)} variant="primary">
+          Create Socket Connection
+        </Button>
+      )}
+      {createSocket && !r && <Spinner />}
+      {error && (
+        <Alert variant="danger" title="Websocket Error">
+          {error}
+        </Alert>
+      )}
+      {r && <PrintObject object={r} />}
+    </>
+  );
 };
 
 export default WSTest;
