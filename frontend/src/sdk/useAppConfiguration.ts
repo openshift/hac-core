b7fb3f117d04470aef7b293f678fd22a9e0e5f9d
Karel Hala <khala@redhat.com>
2022-04-08 13:08:48 +0200

    Use function when using the WS config

--- frontend/src/sdk/useAppConfiguration.ts
+++ frontend/src/sdk/useAppConfiguration.ts
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { commonFetch } from './commonFetch';
 import { getWSTokenSubProtocols } from './wsConfigs';
-import useAuthToken from './useAuthToken';
 import { AppInitSDK } from '@openshift/dynamic-plugin-sdk-utils';
 import { pluginStore } from 'Sdk/createStore';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
@@ -11,22 +10,24 @@ type AppConfigurations = React.ComponentProps<typeof AppInitSDK>['configurations
 const useAppConfiguration = (): AppConfigurations | null => {
   const { auth } = useChrome();
   const [appConfigurations, setAppConfigurations] = React.useState<AppConfigurations | null>(null);
-  const token = useAuthToken();
 
   React.useEffect(() => {
-    if (auth && token && !appConfigurations) {
+    if (auth && !appConfigurations) {
       setAppConfigurations({
         appFetch: commonFetch(auth),
-        wsAppSettings: {
-          host: K8S_WS_TARGET_URL,
-          subProtocols: getWSTokenSubProtocols(token),
-          // TODO: check for `?` and use '&'
-          urlAugment: (url) => `${url}?watch=true`,
+        wsAppSettings: async () => {
+          const token = await auth.getToken();
+          return {
+            host: K8S_WS_TARGET_URL,
+            subProtocols: getWSTokenSubProtocols(token),
+            // TODO: check for `?` and use '&'
+            urlAugment: (url) => `${url}?watch=true`,
+          };
         },
         pluginStore,
       });
     }
-  }, [appConfigurations, pluginStore, token, auth]);
+  }, [appConfigurations, pluginStore, auth]);
 
   return appConfigurations;
 };
