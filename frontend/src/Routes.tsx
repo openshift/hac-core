cb257cf384ef2e0b1b2738ef9d5904dbfac2dd22
Bryan Florkiewicz <bflorkie@redhat.com>
2023-01-09 15:22:32 -0500

    Remove the testK8s page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,7 +4,6 @@ import { Route, Routes as DomRoutes } from 'react-router-dom';
 import { Bullseye, Spinner } from '@patternfly/react-core';
 
 const DynamicRoute = React.lazy(() => import(/* webpackChunkName: "DynamicRoute" */ './Routes/DynamicRoute/DynamicRoute'));
-const TestK8s = React.lazy(() => import(/* webpackChunkName: "TestK8s" */ './Routes/testK8s/TestK8s'));
 
 export const Routes: React.FC = () => (
   <React.Suspense
@@ -15,8 +14,6 @@ export const Routes: React.FC = () => (
     }
   >
     <DomRoutes>
-      <Route path="/hac/testK8s" element={<TestK8s />} />
-      <Route path="/testK8s" element={<TestK8s />} />
       <Route path="/hac/*" element={<DynamicRoute />} />
       <Route path="/*" element={<DynamicRoute />} />
     </DomRoutes>
--- frontend/src/Routes/testK8s/models.ts
+++
@@ -1,8 +0,0 @@
-import { K8sModelCommon } from '@openshift/dynamic-plugin-sdk-utils';
-
-export const ApplicationModel: K8sModelCommon = {
-  apiVersion: 'v1alpha1',
-  kind: 'Application',
-  apiGroup: 'appstudio.redhat.com',
-  plural: 'applications',
-};
