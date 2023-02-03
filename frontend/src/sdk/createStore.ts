faaef4ec50864b9b983e340cb7cf0d90308ae365
Martin Maroši <marvusm.mmi@gmail.com>
2023-02-03 09:52:10 +0100

    Add fallback pluginStore if scalprum is undefined.

--- frontend/src/sdk/createStore.ts
+++ frontend/src/sdk/createStore.ts
@@ -1,13 +1,26 @@
 import { getActivePlugins } from '../Utils/plugins';
 import packageInfo from '../../package.json';
-import { PluginStore } from '@openshift/dynamic-plugin-sdk';
+import { PluginLoader, PluginStore } from '@openshift/dynamic-plugin-sdk';
 
-export const createStore = (store: PluginStore) => {
+export const createStore = (store?: PluginStore) => {
+  let internalStore = store;
+  if (!internalStore) {
+    const loader = new PluginLoader({
+      postProcessManifest: (manifest) => ({
+        ...manifest,
+        loadScripts: manifest.loadScripts ?? ['plugin-entry.js'],
+        registrationMethod: manifest.registrationMethod ?? 'callback',
+      }),
+    });
+    loader.registerPluginEntryCallback();
+    internalStore = new PluginStore();
+    internalStore.setLoader(loader);
+  }
   getActivePlugins(window.insights.chrome.isBeta(), packageInfo.insights.appname).then((data) => {
     data.forEach(({ name: item, pathPrefix = '/api/plugins' }) => {
       const url = `/beta${pathPrefix}/${item}/`;
-      store.loadPlugin(url);
+      internalStore!.loadPlugin(url);
     });
   });
-  return store;
+  return internalStore;
 };
