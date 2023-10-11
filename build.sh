7315d0843815dfd59957c86e8ce5b0aa1514256b
Karel Hala <khala@redhat.com>
2023-10-11 08:59:43 +0200

    Remove caddy file and docker file as they are not needed for the build

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 set -exuo pipefail
 
+export IS_PR=${IS_PR:-false}
+
 pushd frontend
 yarn install
-yarn build
+
+# let's turn on universal build
+if [ "$IS_PR" == true ]; then
+  yarn build
+else
+  export BETA=false
+  yarn build
+  mv dist stable
+  export BETA=true
+  yarn build
+  mv dist preview
+  mkdir -p dist
+  mv stable dist/stable
+  mv preview dist/preview
+fi
+
 popd
